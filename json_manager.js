9ceeae5e7afa9b01fac0e3504524feb3e3a09897
Sam Lazrak <samlazrak@Sams-MBP.T-mobile.com>
2018-11-07 20:38:01 -0600

    commander cli for input file

--- json_manager.js
+++ json_manager.js
@@ -1,23 +1,24 @@
 const fs = require('fs')
 const util = require('util')
+var program = require('commander')
 
 // Created a promise of fs.readFile to mess around
 const readFile = util.promisify(fs.readFile)
 
 // Promise will return data when needed
-async function readJson(file) {
-  return await readFile(file, 'utf8')
+async function readJson(input) {
+  return await readFile(input, 'utf8')
 }
 
-async function writeJson(file) {
+async function writeJson(input) {
   // Like right now!
-  readJson(file).then((data) => {
+  readJson(input).then((data) => {
     // fs.readFile() reads the json in as string, needs to be parsed into an object
     data = JSON.parse(data)
     // Now that it's an object we can use push() to add a new json object to the end of the health_logs array
     data['health_logs'].push({
       type: 'water',
-      amount: '16oz',
+      amount: '32oz',
       date: '2018-11-08T02:00:00+00:00'
     })
     // Afterwards it's neccesary to turn it back into a string for fs.writeFile()
@@ -29,4 +30,9 @@ async function writeJson(file) {
   })
 }
 
-writeJson('input.json')
+// Works with one input but not two, unable to specify output file
+program.command('i <dir>').action((dir) => {
+  writeJson(dir)
+})
+
+program.parse(process.argv)
