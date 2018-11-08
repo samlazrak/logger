d6ba02243ebf91441a974924b7eb1914226d43f4
Sam Lazrak <samlazrak@Sams-MacBook-Pro.local>
2018-11-08 06:45:59 -0600

    cleaned

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const bodyParser = require('body-parser')
 const jsonParser = bodyParser.json()
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
-const database = require('./db')
+const database = require('./objects/db')
 const db = new database(process.env.DB_FILEPATH)
 
 async function readJson(input) {
